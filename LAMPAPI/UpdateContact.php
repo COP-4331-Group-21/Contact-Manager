4470025b123d7d138768cf48484433d7df8a8ecb
Jorge Bulnes <97678857+jbulnes96@users.noreply.github.com>
2022-02-12 02:17:43 -0500

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,30 +1,37 @@
 <?php
-	$inData = getRequestInfo();
 
-	$userId = $inData["userId"];
+	$inData = getRequestInfo();
+	
+	$ID = $inData["ID"];
+	$UserId = $inData["UserId"];
 	$FirstName = $inData["FirstName"];
 	$LastName = $inData["LastName"];
 	$Email = $inData["Email"];
 	$Phone = $inData["Phone"];
-	$today = date("Y-m-d H:i:s"); 
+	$today = date("Y-m-d H:i:s");
 	$DateCreated = $today;
-	
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 
-	if ($conn->connect_error) 
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
-	} 
-	else
-	{	
-		$sql="UPDATE Contacts SET FirstName=$'$FirstName',
-		LastName='$LastName', Email='$Email', Phone='$Phone', WHERE userId=$userId";
-		
 	}
-
+	else
+	{
+		$stmt = $conn->prepare("UPDATE ContactList SET FirstName=?,LastName=?,Email=?,Phone=?, DateCreated=? WHERE ID=? AND UserId=?");
+		$stmt->bind_param("sssssss", $FirstName, $LastName, $Email, $Phone, $DateCreated, $ID ,$UserId);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		
+		$stmt->close();
+		$conn->close();
+	}
+	
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
+
 
 	function sendResultInfoAsJson( $obj )
 	{
@@ -37,5 +44,6 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
+	
 	
 ?>
